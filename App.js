bebf98d4a69597aba6c76c28c69c83550d57d17f
datk1912 <datk1912@gmail.com>
2023-05-16 14:00:14 +0700

    Structure of Web

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-const express = require('express');
+import express from 'express';
+import { engine } from 'express-handlebars';
+
 const app = express();
 
-app.get('/', (req,res) =>{
-    res.send('Hello, World!');
-})
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
-app.listen(3000,() => {
-    console.log('Server is running at port 3000');
-}
-)
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about_us', (req, res) => {
+    res.render('about us');
+});
+
+app.get('/product', (req, res) => {
+    res.render('product');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen((3000), function() {
+    console.log("Sever listen to port 3000");
+});
